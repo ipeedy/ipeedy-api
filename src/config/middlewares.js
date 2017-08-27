b3f970d0004770b1c3972c5c02dfa0324264fc28
Quốc Khánh <bkdev98@gmail.com>
2017-08-28 00:51:12 +0700

    create express server

---
+++ src/config/middlewares.js
@@ -0,0 +1,5 @@
+import bodyParser from 'body-parser';
+
+export default app => {
+  app.use(bodyParser.json());
+}
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import notifier from 'node-notifier';
+
+import constants from './config/constants';
+import middlewares from './config/middlewares';
+
+const app = express();
+
+middlewares(app);
+
+app.listen(constants.PORT, err => {
+  if (err) {
+    console.log(err);
+    notifier.notify({
+      'title': 'Inspery API',
+      'message': 'Service failed to start!',
+    });
+  } else {
+    console.log(`Ipeedy API is up on port ${constants.PORT}!`);
+    notifier.notify({
+      'title': 'Inspery API',
+      'message': 'Service started!',
+    });
+  }
+});
